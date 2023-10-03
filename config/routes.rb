b7391996f5c8073cf73df3fa563a5773ccaac026
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-10-03 22:17:56 +0900

    [Add] トップページのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  
+  # ユーザー側
+  scope module: :public do
+    root to: 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
