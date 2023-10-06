55b65ad44d25942094660a147780c05efee4e5d8
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-10-06 20:22:55 +0900

    [Add] devise, ユーザーコントローラー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  # サインアップ時、ユーザー名のデータ保存を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   
-  # ユーザー側
+  # ユーザー用
   scope module: :public do
+    devise_for :users, skip: [:passwords]
     root to: 'homes#top'
   end
+  
+  # 管理者用
+  namespace :admin do
+    devise_for :admins, skip: [:registrations, :passwords]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
