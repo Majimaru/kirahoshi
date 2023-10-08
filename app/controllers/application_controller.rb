c8308d6addf63e5f44599a50dd2b0de1437f2ba2
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-10-08 11:58:41 +0900

    [BK] 11:58

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_admin!, if: :admin_url
+
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
+  
+  # ログイン後の遷移先
+  def after_sign_in_path_for(resource)
+    case resource
+    
+    when User
+      user_path(current_user)
+    when Admin
+      root_path
+    end
+  end
+  
+  # ログアウト後の遷移先
+  def after_sign_out_path_for(resource)
+    root_path
+  end
   
   protected
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,29 @@
 Rails.application.routes.draw do
   
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+  end
   # ユーザー用
+  devise_for :users,skip: [:passwords], controllers: {
+   registrations: "public/registrations",
+   sessions: 'public/sessions'
+  }
+
   scope module: :public do
-    devise_for :users, skip: [:passwords]
+    # devise_for :users, skip: [:passwords]
     root to: 'homes#top'
+    resources :users, only: [:show, :edit, :update, :destroy]
   end
   
   # 管理者用
-  namespace :admin do
-    devise_for :admins, skip: [:registrations, :passwords]
-  end
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+   sessions: "admin/sessions"
+  }
+  
+  # namespace :admin do
+  #   devise_for :admin, skip: [:registrations, :passwords]
+  # end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
