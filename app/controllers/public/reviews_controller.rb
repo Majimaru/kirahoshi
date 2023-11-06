64b0542b3d5597e8ffddd0ca372543e8c887ccce
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-11-07 06:01:42 +0900

    [Add] レビューコメント　感情分析機能

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -14,14 +14,19 @@ class Public::ReviewsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    # Google Natural Language APIから感情スコアを取得
+    score = Language.get_data(review_params[:comment])
     
     # Reviewテーブルに既にレコードが存在していれば更新、存在しなければ登録
     if @post.reviewed_by?(current_user)
       Review.find_by(user_id: current_user.id, post_id: @post.id).update(review_params)
+      # 感情スコアを更新
+      Review.find_by(user_id: current_user.id, post_id: @post.id).update(emotion_score: score)
       
     else
       review = current_user.reviews.new(review_params)
       review.post_id = @post.id
+      review.emotion_score = score
       review.save
     end
     
