02fd4abdb82765b7b5d97697fa8094c99370c4ec
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-11-08 04:37:11 +0900

    [Fix] デプロイエラー対応

--- db/migrate/20231012123818_create_posts.rb
+++ db/migrate/20231012123818_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      # t.bigint :user_id, null: false
-      t.references :user, null: false, foreign_key: true
-      # t.bigint :genre_id, null: false
-      t.references :genre, null: false, foreign_key: true
+      t.bigint :user_id, null: false
+      # t.references :user, null: false, foreign_key: true
+      t.bigint :genre_id, null: false
+      # t.references :genre, null: false, foreign_key: true
       t.text :message, null: false
       t.string :review_item, null: false
       t.float :average_rate, null: false, default: 0.0
